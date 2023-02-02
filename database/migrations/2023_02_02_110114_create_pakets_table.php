f61c9700e1dcfbeec5611d4c15b1f30f6e33c276
RevaldyPutra <reyreyval9@gmail.com>
2023-02-02 19:39:08 +0700

    laundryfikri

---
+++ database/migrations/2023_02_02_110114_create_pakets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pakets', function (Blueprint $table) {
+            $table->id();
+            //$table-unsignedBigInteger('outlets_id);
+            $table->foreignId('outlet_id')->constrained('outlets');
+            $table->enum('jenis', ['kiloan', 'selimut', 'bed_cover', 'kaos', 'lain']);
+            $table->string('nama_paket', 100);
+            $table->string('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pakets');
+    }
+}
